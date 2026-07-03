edf871e46fd72b9a9eca15c4efeac3161edd41ba
Axel Karlsson <git@axelkarlsson.se>
2026-07-03 12:29:32 +0000

    remove unnecessary obfuscation method which adds no security

--- src/cipher.py
+++ src/cipher.py
@@ -5,35 +5,6 @@ import pyperclip
 encryption_key = Fernet.generate_key()
 cipher_suite = Fernet(encryption_key)
 
-def is_even(number):
-    return number % 2 == 0
-
-def get_even_letters(message):
-    even_letters = []
-    for counter in range(0, len(message)):
-        if is_even(counter):
-            even_letters.append(message[counter])
-    return even_letters
-
-def get_odd_letters(message):
-    odd_letters = []
-    for counter in range(0, len(message)):
-        if not is_even(counter):
-            odd_letters.append(message[counter])
-    return odd_letters
-
-def swap_letters(message):
-    letter_list = []
-    if not is_even(len(message)):
-        message = message + 'x'
-    even_letters = get_even_letters(message)
-    odd_letters = get_odd_letters(message)
-    for counter in range(0, int(len(message)/2)):
-        letter_list.append(odd_letters[counter])
-        letter_list.append(even_letters[counter])
-    new_message = ''.join(letter_list)
-    return new_message
-
 def encrypt_with_cryptography(message):
     encrypted_message = cipher_suite.encrypt(message.encode())
     return encrypted_message.decode()
@@ -60,16 +31,14 @@ while True:
     task = get_task()
     if task == 'encrypt':
         message = get_message()
-        swapped = swap_letters(message)
-        encrypted = encrypt_with_cryptography(swapped)
+        encrypted = encrypt_with_cryptography(message)
         messagebox.showinfo('Ciphertext of the secret message is: ', encrypted)
         copy_choice = messagebox.askyesno('Copy?', 'Do you want to copy this to clipboard?')
         if copy_choice:
             copy_to_clipboard(encrypted)
     elif task == 'decrypt':
         message = get_message()
-        decrypted_crypto = decrypt_with_cryptography(message)
-        decrypted = swap_letters(decrypted_crypto)
+        decrypted = decrypt_with_cryptography(message)
         messagebox.showinfo('Plaintext of the secret message is: ', decrypted)
     else:
         break

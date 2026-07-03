88339edb163577cc93070c8eab1c84f412618d22
Axel Karlsson <git@axelkarlsson.se>
2026-07-03 12:36:35 +0000

    use black code formatter

--- src/cipher.py
+++ src/cipher.py
@@ -5,42 +5,50 @@ import pyperclip
 encryption_key = Fernet.generate_key()
 cipher_suite = Fernet(encryption_key)
 
+
 def encrypt_with_cryptography(message):
     encrypted_message = cipher_suite.encrypt(message.encode())
     return encrypted_message.decode()
+
 
 def decrypt_with_cryptography(encrypted_message):
     decrypted_message = cipher_suite.decrypt(encrypted_message.encode())
     return decrypted_message.decode()
 
+
 def copy_to_clipboard(message):
     pyperclip.copy(message)
-    messagebox.showinfo('Success', 'Message copied to clipboard!')
+    messagebox.showinfo("Success", "Message copied to clipboard!")
+
 
 def get_task():
-    task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt?')
+    task = simpledialog.askstring("Task", "Do you want to encrypt or decrypt?")
     return task
 
+
 def get_message():
-    message = simpledialog.askstring('Message', 'Enter the secret message: ')
+    message = simpledialog.askstring("Message", "Enter the secret message: ")
     return message
+
 
 def main():
     root = Tk()
 
     while True:
         task = get_task()
-        if task == 'encrypt':
+        if task == "encrypt":
             message = get_message()
             encrypted = encrypt_with_cryptography(message)
-            messagebox.showinfo('Ciphertext of the secret message is: ', encrypted)
-            copy_choice = messagebox.askyesno('Copy?', 'Do you want to copy this to clipboard?')
+            messagebox.showinfo("Ciphertext of the secret message is: ", encrypted)
+            copy_choice = messagebox.askyesno(
+                "Copy?", "Do you want to copy this to clipboard?"
+            )
             if copy_choice:
                 copy_to_clipboard(encrypted)
-        elif task == 'decrypt':
+        elif task == "decrypt":
             message = get_message()
             decrypted = decrypt_with_cryptography(message)
-            messagebox.showinfo('Plaintext of the secret message is: ', decrypted)
+            messagebox.showinfo("Plaintext of the secret message is: ", decrypted)
         else:
             break
         root.mainloop
